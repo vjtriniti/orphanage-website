0b99f5a9cfc00f1609adf5eee1c3b16b9a87d78c
vjtriniti <v7045612@gmail.com>
2026-08-31 21:04:40 -0700

    feat: add admin authorization middleware

---
+++ app/Http/Middleware/EnsureAdminAccess.php
@@ -0,0 +1,4 @@
+<?php
+namespace App\Http\Middleware;
+use Closure; use Illuminate\Http\Request; use Symfony\Component\HttpFoundation\Response;
+class EnsureAdminAccess { public function handle(Request $request, Closure $next): Response { abort_unless($request->user()?->is_admin === true || $request->user()?->role === 'admin',403); return $next($request); } }

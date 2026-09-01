9d7beeb943abec7e4d7420b34868b5e8d641b73c
vjtriniti <v7045612@gmail.com>
2026-08-31 21:21:30 -0700

    feat: add simple admin authorization middleware

---
+++ app/Http/Middleware/EnsureAdmin.php
@@ -0,0 +1,4 @@
+<?php
+namespace App\Http\Middleware;
+use Closure; use Illuminate\Http\Request;
+class EnsureAdmin { public function handle(Request $request, Closure $next){$user=$request->user(); abort_unless($user && ($user->is_admin ?? false),403); return $next($request); } }

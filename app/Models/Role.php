c9c6c9ae582c0032d164f599734fc747e29abb27
vjtriniti <v7045612@gmail.com>
2026-09-03 02:05:10 -0700

    feat: add role model

---
+++ app/Models/Role.php
@@ -0,0 +1,4 @@
+<?php
+namespace App\Models;
+use Illuminate\Database\Eloquent\Model;
+class Role extends Model { protected $fillable=['name','label']; public function users(){return $this->belongsToMany(User::class);} public function permissions(){return $this->belongsToMany(Permission::class);} }

829268ea73f55ca28e6dc28085ad0e73a81870bd
vjtriniti <v7045612@gmail.com>
2026-09-03 20:48:36 -0700

    Restore queue configuration

---
+++ config/queue.php
@@ -0,0 +1,3 @@
+<?php
+
+return ['default'=>env('QUEUE_CONNECTION','database'),'connections'=>['sync'=>['driver'=>'sync'],'database'=>['driver'=>'database','connection'=>env('DB_QUEUE_CONNECTION'),'table'=>env('DB_QUEUE_TABLE','jobs'),'queue'=>env('DB_QUEUE','default'),'retry_after'=>90,'after_commit'=>false]],'batching'=>['database'=>env('DB_CONNECTION','mysql'),'table'=>'job_batches'],'failed'=>['driver'=>'database-uuids','database'=>env('DB_CONNECTION','mysql'),'table'=>'failed_jobs']];

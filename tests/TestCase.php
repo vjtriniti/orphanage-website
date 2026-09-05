381b0143b970b31ccd5bb852d27186470bee3560
vjtriniti <v7045612@gmail.com>
2026-09-04 20:26:53 -0700

    Add Laravel test base class

---
+++ tests/TestCase.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Tests;
+
+use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+
+abstract class TestCase extends BaseTestCase
+{
+    // Shared Laravel feature-test base class.
+}
